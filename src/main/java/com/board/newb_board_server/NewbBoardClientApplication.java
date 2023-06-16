ac055d23257a35e6d2ebbf8e2ea6386f38dfc765
gomysw1202 <gomysw1202@gmail.com>
2023-06-16 16:51:15 +0900

    Initial commit

---
+++ src/main/java/com/board/newb_board_server/NewbBoardClientApplication.java
@@ -0,0 +1,13 @@
+package com.board.newb_board_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewbBoardClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewbBoardClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/board/newb_board_server/NewbBoardClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.board.newb_board_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewbBoardClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
